766529ebab12f0bd97fce826da7e3988568521f3
huayubo-0701 <huayub@kean.edu>
2026-09-24 16:04:53 -0400

    Calculate drink and pastry revenue

--- assignment1_salesAnalysis/sales_analysis.py
+++ assignment1_salesAnalysis/sales_analysis.py
@@ -3,3 +3,5 @@ number_of_drinks_sold = 1541
 price_per_drink = 3.75
 number_of_pastries_sold = 1031
 price_per_pastry = 3.75
+drink_revenue = number_of_drinks_sold * price_per_drink
+pastry_revenue = number_of_pastries_sold * price_per_pastry
