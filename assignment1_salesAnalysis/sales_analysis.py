8ca59e475b323c63baf8a319e3673be27544e6ff
huayubo-0701 <huayub@kean.edu>
2026-09-24 16:46:13 -0400

    Print written analysis file

--- assignment1_salesAnalysis/sales_analysis.py
+++ assignment1_salesAnalysis/sales_analysis.py
@@ -5,3 +5,7 @@ number_of_pastries_sold = 1031
 price_per_pastry = 3.75
 drink_revenue = number_of_drinks_sold * price_per_drink
 pastry_revenue = number_of_pastries_sold * price_per_pastry
+
+with open("sales_analysis.txt", "r") as file:
+    written_analysis = file.read()
+    print(written_analysis)
