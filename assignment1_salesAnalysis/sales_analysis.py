26446e2a835270c97e417bd6446c925bc8db04ff
huayubo-0701 <huayub@kean.edu>
2026-09-24 16:49:56 -0400

    Add revenue threshold check
    Added total revenue calculation and conditional print statements.

--- assignment1_salesAnalysis/sales_analysis.py
+++ assignment1_salesAnalysis/sales_analysis.py
@@ -5,7 +5,13 @@ number_of_pastries_sold = 1031
 price_per_pastry = 3.75
 drink_revenue = number_of_drinks_sold * price_per_drink
 pastry_revenue = number_of_pastries_sold * price_per_pastry
+total_revenue = drink_revenue + pastry_revenue
 
 with open("sales_analysis.txt", "r") as file:
     written_analysis = file.read()
     print(written_analysis)
+
+if total_revenue >= 500:
+    print("Total revenue is at least $500.")
+else:
+    print("Total revenue is less than $500.")
